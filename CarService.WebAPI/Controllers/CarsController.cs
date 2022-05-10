d6de72dea8c67ddb86a3d02a98cd7cc6607846f3
Git Server Admin <admin@git-server>
2022-05-10 16:50:31 +0000

    Add initial repository

---
+++ CarService.WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarService.WebAPI.Data;
+using CarService.WebAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CarService.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarsController : ControllerBase
+    {
+        private readonly ICarsService _carsService;
+
+        public CarsController(ICarsService carsService)
+        {
+            _carsService = carsService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var user = (await _carsService.Get(new[] { id }, null)).FirstOrDefault();
+            if (user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAll([FromQuery] Filters filters)
+        {
+            var cars = await _carsService.Get(null, filters);
+
+            return Ok(cars);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Car car)
+        {
+            await _carsService.Add(car);
+            return Ok(car);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = (await _carsService.Get(new[] { id }, null)).FirstOrDefault();
+            if (user == null)
+                return NotFound();
+
+            await _carsService.Delete(user);
+            return NoContent();
+        }
+    }
+}
